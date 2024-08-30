61cdf69dd0d75fed85633a1ae33d882572214ddf
Martin <martinsutch@gmail.com>
2024-08-30 12:06:27 +0100

    Adds get user by username endpoint

--- routers/users-router.js
+++ routers/users-router.js
@@ -1,8 +1,12 @@
 const usersRouter = require("express").Router();
-const { getUsers } = require("../controllers/users-controller");
+const { getUsers, getUserByUsername } = require("../controllers/users-controller");
 
 usersRouter
     .route("/")
     .get(getUsers);
+
+usersRouter
+    .route("/:username")
+    .get(getUserByUsername);
 
 module.exports = usersRouter;
